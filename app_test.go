685e4da276a225652a43741e9f1633d136b4dcc5
Rishav Sharan <rishav.sharan@live.com>
2018-11-29 12:47:43 +0530

    adding simple test 26

--- app_test.go
+++ app_test.go
@@ -14,3 +14,23 @@ func TestIncrementCase2(t *testing.T) {
 		t.Error("Expected increment of 5 to be 7")
 	}
 }
+
+func TestIncrement(t *testing.T) {
+	tables := []struct {
+		x int
+		y int
+	}{
+		{1, 2},
+		{2, 3},
+		{3, 4},
+		{4, 6},
+		{5, 7},
+	}
+
+	for _, table := range tables {
+		res := Increment(table.x)
+		if res != table.n {
+			t.Errorf("Increment of (%d) was incorrect returned as (%d) instead of (%d)!", table.x, table.y, res)
+		}
+	}
+}
